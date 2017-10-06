0c363b256372325927b1810972879a46159eab20
Sam Cappleman-Lynes <Sam.Cappleman-Lynes@softwire.com>
2017-10-06 13:40:54 +0100

    Add a test for pawns moving two spaces initially done red-2

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -9,19 +9,30 @@ export default class Pawn extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
+        const baseWhitePawnLine : number = 1;
+        const baseBlackPawnLine : number = 6;
         const availableMoves: Square[] = [];
         const currentPosition: Square = board.findPiece(this);
+        
         switch (this.player) {
             case Player.WHITE:
                 const normalWhitePawnMove : Square = Square.at(currentPosition.row + 1, currentPosition.col)
+                const doubleInitialMoveWhite : Square | null = currentPosition.row == baseWhitePawnLine ? Square.at(currentPosition.row + 2, currentPosition.col) : null;
                 if (board.getPiece(normalWhitePawnMove) == null) {
                     availableMoves.push(normalWhitePawnMove);
                 }
+                if (doubleInitialMoveWhite != null) {
+                    availableMoves.push(doubleInitialMoveWhite);
+                }
                 break;
             case Player.BLACK:
-                const normalBlackPawnMove : Square = Square.at(currentPosition.row - 1, currentPosition.col)
+                const normalBlackPawnMove : Square = Square.at(currentPosition.row - 1, currentPosition.col);
+                const doubleInitialMoveBlack : Square | null = currentPosition.row == baseBlackPawnLine ? Square.at(currentPosition.row - 2, currentPosition.col) : null;
                 if (board.getPiece(normalBlackPawnMove) == null) {
                     availableMoves.push(normalBlackPawnMove);
+                }
+                if (doubleInitialMoveBlack != null) {
+                    availableMoves.push(doubleInitialMoveBlack);
                 }
                 break
             default:
