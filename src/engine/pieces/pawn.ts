7d8b1133e21f9fc1f5fe03ee800ecf193a746609
Matei Stefanica <matei.stefanica@softwire.com>
2026-07-10 13:53:22 +0300

    done red-1

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from '../square';
 
 export default class Pawn extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,26 @@ export default class Pawn extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const availableMoves: Square[] = [];
+        const currentPosition: Square = board.findPiece(this);
+        switch (this.player) {
+            case Player.WHITE:
+                const normalWhitePawnMove : Square = Square.at(currentPosition.row + 1, currentPosition.col)
+                if (board.getPiece(normalWhitePawnMove) == null) {
+                    availableMoves.push(normalWhitePawnMove);
+                }
+                break;
+            case Player.BLACK:
+                const normalBlackPawnMove : Square = Square.at(currentPosition.row - 1, currentPosition.col)
+                if (board.getPiece(normalBlackPawnMove) == null) {
+                    availableMoves.push(normalBlackPawnMove);
+                }
+                break
+            default:
+                // maybe add error later
+                console.log("Unkown player!")
+                break
+        }
+        return availableMoves;
     }
 }
