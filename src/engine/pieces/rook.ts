1dcd70b33a839d723994db54bcf49de6e8305ef1
Matei Stefanica <matei.stefanica@softwire.com>
2026-07-10 16:34:53 +0300

    done red-9

--- src/engine/pieces/rook.ts
+++ src/engine/pieces/rook.ts
@@ -11,14 +11,40 @@ export default class Rook extends Piece {
     public getAvailableMoves(board: Board) {
         const availableMoves : Square[] = [];
         const currentPosition : Square = board.findPiece(this);
-        for (let i = 0; i < 8; i++) {
-            if (i != currentPosition.col) {
-                availableMoves.push(Square.at(currentPosition.row, i))
+
+        for (let i = currentPosition.row - 1; i >= 0; i--) {
+            if (board.getPiece(Square.at(i, currentPosition.col)) == undefined) {
+                availableMoves.push(Square.at(i, currentPosition.col));
+            }
+            else {
+                break;
             }
         }
-        for (let i = 0; i < 8; i++) {
-            if (i != currentPosition.row) {
-                availableMoves.push(Square.at(i, currentPosition.col))
+
+        for (let i = currentPosition.row + 1; i < 8; i++) {
+            if (board.getPiece(Square.at(i, currentPosition.col)) == undefined) {
+                availableMoves.push(Square.at(i, currentPosition.col));
+            }
+            else {
+                break;
+            }
+        }
+
+        for (let i = currentPosition.col - 1; i >= 0; i--) {
+            if (board.getPiece(Square.at(currentPosition.row, i)) == undefined) {
+                availableMoves.push(Square.at(currentPosition.row, i));
+            }
+            else {
+                break;
+            }
+        }
+
+        for (let i = currentPosition.col + 1; i < 8; i++) {
+            if (board.getPiece(Square.at(currentPosition.row, i)) == undefined) {
+                availableMoves.push(Square.at(currentPosition.row, i));
+            }
+            else {
+                break;
             }
         }
         return availableMoves;
